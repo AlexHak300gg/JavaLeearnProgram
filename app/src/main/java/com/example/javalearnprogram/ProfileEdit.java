c17fa260e7b1ae0e0c91fe0e1ceb577494d5ac4a
AlexHak300gg <comp321089@mail.ru>
2024-12-05 19:30:43 +0300

    go

--- app/src/main/java/com/example/javalearnprogram/ProfileEdit.java
+++ app/src/main/java/com/example/javalearnprogram/ProfileEdit.java
@@ -1,24 +1,36 @@
 package com.example.javalearnprogram;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import com.example.javalearnprogram.R;
 
 public class ProfileEdit extends AppCompatActivity {
 
+    Button editingProfileButton;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_profile_edit);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        editingProfileButton = findViewById(R.id.EditingProfileButton);
+
+        editingProfileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfileEdit.this, ProfileEdit.class);
+                startActivity(intent);
+            }
         });
     }
 }
